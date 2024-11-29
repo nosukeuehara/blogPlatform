a8cd52fc9b7a0f989dd5bbf40cbc38efc023477c
DE-Uehara <r.uehara@defide-ix.com>
2024-11-29 12:19:41 +0900

    ページ内の要素を作成

--- src/app/(page)/articles/[postid]/edit/layout.tsx
+++ src/app/(page)/articles/[postid]/edit/layout.tsx
@@ -1,11 +1,17 @@
+import EditorHeder from "@/components/layouts/EditorHeder";
 import React from "react";
 
+// 記事編集ページ
 const layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <EditorHeder /> {children}
+    </div>
+  );
 };
 
 export default layout;
---
+++ src/components/layouts/EditorHeder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ToggleSwitch from "../elements/TogglePublishSwitch";
+
+const EditorHeder = () => {
+  return (
+    <div>
+      <ToggleSwitch />
+    </div>
+  );
+};
+
+export default EditorHeder;
