0a16c0dd51d9b61cd91737ac266863731a8a60bb
DE-Uehara <r.uehara@defide-ix.com>
2024-12-27 16:39:18 +0900

    修正

--- src/components/elements/popoverButtonSet/PopoverButtonSet.tsx
+++ src/components/elements/popoverButtonSet/PopoverButtonSet.tsx
@@ -1,29 +1,33 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./popoverButtonSet.module.css";
 
 const PopoverButtonSet = ({ articleId }: { articleId: string }) => {
   const [isPopoverState, setPopoverState] = useState<boolean>(false);
+  const popoverRef = useRef<HTMLDivElement | null>(null);
 
-  const popover = () => {
-    if (isPopoverState) {
-      return (
-        <div>
-          <div className={`${styles.popoverMenue} ${styles.fadeInUp}`}>
-            <a rel="noopener noreferrer" target="_blank">
-              {/* 画面をクリックしたらボタンを閉じるようにする */}
-              <button className={styles.deleteBtn}>
-                <span>削除する</span>
-              </button>
-            </a>
-          </div>
-        </div>
-      );
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      popoverRef.current &&
+      !popoverRef.current.contains(event.target as Node)
+    ) {
+      setPopoverState(false);
     }
-    return null;
   };
+
+  useEffect(() => {
+    if (isPopoverState) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isPopoverState]);
+
   return (
-    <div id={articleId} className={styles.menuButtonContainer}>
+    <div id={articleId} className={styles.menuButtonContainer} ref={popoverRef}>
       <button
         className={styles.menuButton}
         onClick={() => {
@@ -32,7 +36,15 @@ const PopoverButtonSet = ({ articleId }: { articleId: string }) => {
       >
         ・・・
       </button>
-      {popover()}
+      {isPopoverState && (
+        <div className={`${styles.popoverMenue} ${styles.fadeInUp}`}>
+          <a rel="noopener noreferrer" target="_blank">
+            <button className={styles.deleteBtn}>
+              <span>削除する</span>
+            </button>
+          </a>
+        </div>
+      )}
     </div>
   );
 };
